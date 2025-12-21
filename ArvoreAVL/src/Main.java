42da8af1d0201205797fb708950b9e612996cdd1
Diego da mata <Diegodamata>
2025-12-21 15:58:00 -0300

    Remocao de um node e mantendo seus filhos e rebalanceando a arvore

--- ArvoreAVL/src/Main.java
+++ ArvoreAVL/src/Main.java
@@ -19,12 +19,21 @@ public class Main {
             opc = sc.nextInt();
         }
 
-
+        inOrder(root);
         System.out.println("Value height tree: " +  root.getHeight());
+
+        System.out.print("Deseja removar um valor da arvore? (1 - Sim / 2 - Não): ");
+        opc = sc.nextInt();
+        if (opc == 1){
+            System.out.print("Informe o valor: ");
+            int value = sc.nextInt();
+            root = remove(root, value);
+        }
+
+        inOrder(root);
 
         sc.close();
     }
-
 
     public static Node<Integer> insert(Node<Integer> no, int value){
         if (no == null){
@@ -43,10 +52,7 @@ public class Main {
 
         no.setHeight(Math.max(altura(no.getLeft()), altura(no.getRight())) + 1);
 
-        System.out.println(no.getValue());
-        Node<Integer> node = verificarRotacao(fatorBalanceamento(no), no);
-
-        return node;
+        return verificarRotacao(fatorBalanceamento(no), no);
     }
 
     private static Node<Integer> verificarRotacao(int fatorBalanceamento, Node<Integer> no) {
@@ -126,6 +132,7 @@ public class Main {
 
         return rotacaoDireita(no);
     }
+
     private static int fatorBalanceamento(Node<Integer> no) {
         if (no != null){
             return altura(no.getLeft()) - altura(no.getRight());
@@ -136,5 +143,55 @@ public class Main {
     public static int altura(Node<Integer> no){
         if (no == null) return -1;
         return no.getHeight();
+    }
+
+    public static void inOrder(Node<Integer> no){
+        if (no == null) return;
+        inOrder(no.getLeft());
+        System.out.println(no.getValue());
+        inOrder(no.getRight());
+    }
+
+    private static Node<Integer> remove(Node<Integer> no, int value) {
+        if (no == null) return null;
+
+        if (no.getValue().equals(value)){
+            if (no.getLeft() == null && no.getRight() == null){
+                return null;
+            }
+            if (no.getLeft() != null && no.getRight() == null){
+                return no.getLeft();
+            }
+            if (no.getLeft() == null && no.getRight() != null){
+                return no.getRight();
+            }
+            if (no.getLeft() != null && no.getRight() != null){
+                Node<Integer> j, k, i = null;
+                j = no.getRight();
+                k = j.getLeft();
+
+                if (k == null){
+                    j.setLeft(no.getLeft());
+                }
+                else {
+                    while (k != null) {
+                        i = k;
+                        k = k.getLeft();
+                    }
+                    i.setLeft(no.getLeft());
+                }
+                return j;
+            }
+        }
+
+        if (value <= no.getValue()){
+            no.setLeft(remove(no.getLeft(), value));
+        }else {
+            no.setRight(remove(no.getRight(), value));
+        }
+
+        no.setHeight(Math.max(altura(no.getLeft()), altura(no.getRight()) + 1));
+
+        return verificarRotacao(fatorBalanceamento(no), no);
     }
 }
